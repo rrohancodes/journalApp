a6b271417537fd5d33df9fc08aab2af4cdea5350
Rohan Kumar <rohan150515454@gmail.com>
2026-08-02 19:03:52 +0530

    lombok

--- JournalApp/src/main/java/com/Journal/JournalApp/Entity/JournalEntry.java
+++ JournalApp/src/main/java/com/Journal/JournalApp/Entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package com.Journal.JournalApp.Entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,6 +8,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.time.LocalDateTime;
 
 @Document
+@Data
 public class JournalEntry {
 
     @Id
@@ -17,36 +19,5 @@ public class JournalEntry {
     private String content;
 
     private LocalDateTime date;
-    
-    public LocalDateTime getDate() {
-        return date;
-    }
 
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
